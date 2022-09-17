6d942674b234fb0e12e9fe37d3b61948ef4c659a
Midnight <kevinrodriguez4851@gmail.com>
2022-09-17 12:12:58 -0600

    install script

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # help 
 drop_about()
 {
-  TITLE="\n[*] Simplicity installer: \n\n"
-  GUIDE_1="-> if you have dependencies ready, then just go with install repo (r)\n"
-  GUIDE_2="-> if you don't have any dependencies, do the install dependencies (d)\n" 
-  NOTE="\n[*] NOTE: this installer require you to have paru"
+  TITLE="\n\e[32m [*] \e[36m Simplicity installer: \n\n"
+  GUIDE_1="\e[36m -> if you have dependencies ready, then just go with install repo \e[32m(r)\n"
+  GUIDE_2="\e[36m -> if you don't have any dependencies, do the install dependencies \e[32m(d)\n" 
+  NOTE="\n\e[32m [*] \e[36mNOTE: this installer requires paru"
 
-  echo -e "$TITLE $GUIDE_1 $GUIDE_2" 
+  echo -e "$TITLE $GUIDE_1 $GUIDE_2 $NOTE" 
 }
 
 # repo installer
@@ -15,14 +15,14 @@ link_files()
 {
   echo -e '\n [*] LINKING FILES. . .'
   ls ~/.dotfiles/ | while read -r ITEM; do  
-    if [ "$ITEM" != 'install.sh' ];
+    if [ "$ITEM" != 'install.sh' ] && [ "$ITEM" != 'README.md' ];
     then
       (ln -s ~/.dotfiles/"$ITEM" ~/.config/) &>/dev/null
       if [ "$(echo $?)" == 0 ]; 
       then
-        echo -e "\n [+] $ITEM simlinked in ~/.config/"
+        echo -e "\n\e[32m [+] $\e[36m ITEM simlinked in ~/.config/"
       else
-        echo -e "\n [!] $ITEM already in ~/.config/"
+        echo -e "\n\e[31m [!] \e[36m $ITEM already in ~/.config/"
       fi
     fi
   done
@@ -31,10 +31,10 @@ link_files()
 # dependencies installer
 install_dependencies()
 {
-  echo -e '\n[*] installing dependencies. . .\n'
-  INSTALL=`paru -S xmonad kitty neovim rofi eww-git thunar nerd-fonts-jetbrains-mono zsh`
+  echo -e ' \n\e[32m [*] \e[36minstalling dependencies . . .\n\e[37m'
+  INSTALL=`paru -S xmonad kitty neovim rofi eww-git thunar nerd-fonts-jetbrains-mono zsh zsh-theme-powerlevel10k-git`
   if [ "$INSTALL" >/dev/null ]; then
-    echo -e '\n[*] CANCELLED\n'
+    echo -e '\n\e[31m[*] \e[36mCANCELLED\n\e[37m'
   else
     link_files
   fi
@@ -43,7 +43,6 @@ install_dependencies()
 # Simplicity installer 
 installer_do()
 {
-  # install menu
   TITLE="\n\e[32m [*] Simplicity installer: \n\n"
   OPTION_1="\e[32m(r) \e[36minstall repo\n"
   OPTION_2="\e[32m(d) \e[36minstall dependencies\n"
@@ -63,13 +62,14 @@ installer_do()
     drop_about
   else 
 
-    echo -e '\n[*] No valid input!'
+    echo -e '\n [*] No valid input!'
   fi
 }
 
 # check for no super user
-if [ "$USER" == 'root' ]; then
-  echo -e "\n[*] ERROR \n\n--> Root user not allowed"
+ISROOT=`whoami`
+if [ "$ISROOT" == 'root' ]; then
+  echo -e "\n\e[31m [*] ERROR \e[32m\n\n -> Root user not allowed\n\e[37m"
 else
   installer_do
 fi
