2fdb56a8926c1cad0a0e2bde9e2e9354618c3437
midnight <kevinrodriguez4851@gmail.com>
2022-10-05 20:14:38 -0600

    colorscheme

---
+++ nvim/lua/starry_night/palette.lua
@@ -0,0 +1,32 @@
+
+local colors = {
+
+-----------------------------
+--------- coloring ----------
+-----------------------------
+
+  alt = "#262026",
+  err = "#ff6e6e",
+
+  _c0 = "#0f080e",
+  _c1 = "#68095c",
+  _c2 = "#c9c9c9",
+
+  _c3 = "#6c686c",
+  _c4 = "#3c0835",
+  _c5 = "#986992",
+
+  _c6 = "#563d53",
+  _c7 = "#82687f",
+  _c8 = "#6a3864",
+
+  error_bg = "#ffffff",
+  warn_bg = "#ffffff",
+  info_bg = "#ffffff",
+  hint_bg = "#ffffff",
+  success_green = "#55FF55",
+  folder_blue = "#55FFFF",
+
+}
+
+return colors
